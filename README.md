107265b65fad952742170f512667a38b295b979f
osenberg <osenberg@qq.com>
2022-01-21 11:30:24 +0800

    completed README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # algorithm
 Implemention of basic algorithm and data structure by Rust.
+
+# Usage
+## Show Usage
+```
+cargo run
+```
+## Insertion Sort
+```
+cargo run --example insertion_sort
+```
+## Binary Search
+```
+cargo run --example binary_search
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Implemention of algorithm by Rust!");
+    println!("\nImplemention of algorithm by Rust!");
+    println!("\nUsage:");
+    println!("Insertion Sort:\t\tcargo run --example insertion_sort");
+    println!("Binary Search:\t\tcargo run --example binary_search");
 }
